f7709348593384c6089375adb1b2d26eed646909
Anshik Jaiswal <anshik0706@gmail.com>
2025-02-25 16:50:22 +0530

    [Anshik] Add. added dto and model

---
+++ src/main/java/com/uc/employeepayroll/model/EmployeeModel.java
@@ -0,0 +1,22 @@
+package com.uc.employeepayroll.model;
+
+public class EmployeeModel {
+    private String name;
+    private double salary;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
