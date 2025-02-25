afb4debe0066678d03c510759ce83a3847b807c9
Anshik Jaiswal <anshik0706@gmail.com>
2025-02-25 16:29:39 +0530

    [Anshik] Add. created controller and set mysql credential

--- src/main/java/com/uc/employeepayroll/controller/EmployeeController.java
+++ src/main/java/com/uc/employeepayroll/controller/EmployeeController.java
@@ -3,16 +3,33 @@ package com.uc.employeepayroll.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
-    @GetMapping("/employee")
-    public ResponseEntity<String> getEmployee(@RequestParam(required = false, defaultValue = "Guest") String name){
-        return new ResponseEntity<>("Welcome " + name, HttpStatus.OK);
+    @GetMapping("/all")
+    public ResponseEntity<String> getAllEmployee(){
+        return new ResponseEntity<>("All employees are : " , HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getEmployee(@PathVariable Long id){
+        return new ResponseEntity<>("Get employee ID: " + id, HttpStatus.OK);
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<String> postEmployee(@RequestBody Object employee){
+        return new ResponseEntity<>("Employee created : ", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/put/{id}")
+    public ResponseEntity<String> putEmployee(@PathVariable Long id){
+        return new ResponseEntity<>("Updated employee: " + id, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+        return new ResponseEntity<>("Deleted employee : " + id, HttpStatus.OK);
+    }
 }
