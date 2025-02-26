f2bf6b5737c4c5ac4887469b5399227020ec8956
Anshik Jaiswal <anshik0706@gmail.com>
2025-02-26 19:15:15 +0530

    [Anshik] Add. Using lombok in dto and model

--- src/main/java/com/uc/employeepayroll/dto/EmployeeDTO.java
+++ src/main/java/com/uc/employeepayroll/dto/EmployeeDTO.java
@@ -1,31 +1,15 @@
 package com.uc.employeepayroll.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// using getter setter annotation to access getter and setter for the private attribute
+@Getter
+@Setter
 public class EmployeeDTO {
+
     private Long id;
     private String name;
     private double salary;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
